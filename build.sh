415f788984c1b4b54bfe5834f471eb5e1e479fab
spicylemonade <84095175+spicylemonade@users.noreply.github.com>
2022-07-16 03:40:17 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -21,31 +21,27 @@ fi
      mkdir ~/.punch && mkdir ~/.punch/trash && mkdir ~/.punch/bin && echo "~/.punch created in home"
 
 
+cargo build  --release \
+ && echo "succefully compiled"
 
-(
-     cargo build  --release \
-     && echo "succefully compiled" && \
-     mv -f ./target/release/punch ~/.punch/bin/
-     (
-          printf '\n #alias to punch \n alias punch="~/.punch/bin/punch"' >> ~/.zshrc \
-          && printf '\n #alias to punch \n alias punch="~/.punch/bin/punch"' >> ~/.bashrc \
-          && . ~/.zshrc \
-          && . ~/.bashrc
-     )||(
-          printf '\n #alias to punch \n alias punch="~/.punch/bin/punch"' >> ~/.bashrc \
-          && echo "done" \
-          && . ~/.bashrc 
-     )||(
-          printf '\n #alias to punch \n alias punch="~/.punch/bin/punch"' >> ~/.zshrc \
-          && . ~/.zshrc
-     ) && \
-     \
-     echo "puch located at ~/.punch" \
-     && echo "complete: type punch -h" &&
-)||(
-     echo "failed: if you are on debian cc build essentials must be installed, run :" \
+mv ./target/release/punch ~/.punch/bin/
+
+{
+    printf 'alias punch="~/.punch/bin/punch"' >> ~/.zshrc \
+    && printf 'alias punch="~/.punch/bin/punch"' >> ~/.bashrc \
+    && . ~/.zshrc \
+    && . ~/.bashrc 
+
+}||{
+     printf 'alias punch="~/.punch/bin/punch"' >> ~/.bashrc \
+     && . ~/.bashrc
+}||{
+     printf 'alias punch="~/.punch/bin/punch"' >> ~/.zshrc \
+     && . ~/.zshhrc
+}||{
+   echo "failed: if you are on debian cc build essentials must be installed, run :" \
      \
      && echo 'sudo apt update && sudo apt upgrade && sudo apt-get install build-essential' \
      \
      && echo 'after running, try building again'
-)
+}
