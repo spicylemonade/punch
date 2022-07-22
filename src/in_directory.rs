dd2abd5e82eb6aeb56f1583ad1099c57d1e16144
Mayowa <mayowadpastevans@yahoo.co.uk>
2022-07-22 22:44:13 +0100

    Added the feature for trashing files or directories

--- src/in_directory.rs
+++ src/in_directory.rs
@@ -7,7 +7,7 @@ pub fn create_in_dir(args: &Args) {
     for i in 0..args.len() {
         if args[i].contains("/") {
             fs::create_dir_all(format!("{}{}", args[2], args[i]))
-                .expect(format!("error creating folder: {}", args[i]).as_str());
+                .expect(format!("error creating directory: {}", args[i]).as_str());
         } else {
             fs::File::create(format!("{}/{}", args[2], args[i]))
                 .expect(format!("error creating file: {}", args[i]).as_str());
@@ -20,10 +20,10 @@ pub fn delete_files_dir(args: &Args) {
     for i in 1..args.len() {
         if args[i].contains("/") {
             fs::remove_dir_all(format!("{}{}", args[2], args[i]))
-                .expect(format!("error creating folder: {}", args[i]).as_str());
+                .expect(format!("error removing directory: {}", args[i]).as_str());
         } else {
             fs::remove_file(format!("{}/{}", args[2], args[i]))
-                .expect(format!("error creating folder: {}", args[i]).as_str());
+                .expect(format!("error removing directory: {}", args[i]).as_str());
         }
     }
 }
