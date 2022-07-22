b8d4442dbbc55f8f18b89b1ad659511fcd44b891
Ishaan Subramanya <whthownothing@gmail.com>
2022-07-22 17:49:41 +0530

    rewritten in clap

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,87 @@
-use std::env;
 use std::fs;
 
+use clap::Parser;
+
 mod in_directory;
-fn help_message(){
-    println!("punch (optional)<flag> <file/location> \n
-    
-    punch -h 'to bring up help' \n
-    punch <file_name> 'to create file' \n
-    punch -dir ./<directory_name>/ 'create directory' \n
-    punch ./<directory_name>/ 'to use without -dir flag' \n
-    punch -d <file_name> 'or'  punch -d ./<directory_name>/ 'to delete' \n
-    punch -in ./<target_directory_name>/ <file or directory_name> 'creates files inside target directory' \n
-    punch -din ./<target_directory_name>/ <file or directory_name> 'deletes files inside target directory' \n
-    ")
+
+#[derive(Debug, Parser)]
+#[clap(trailing_var_arg = true)]
+pub struct Args {
+    /// to create file
+    #[clap(short, value_parser, multiple_values = true)]
+    target: Option<Vec<String>>,
+    /// to delete
+    #[clap(short, long, value_parser)]
+    del: Option<Vec<String>>,
+    /// creates files inside target directory
+    #[clap(short, long, value_parser, multiple_values = true)]
+    r#in: Option<Vec<String>>,
+    /// deletes files inside target directory
+    #[clap(long, value_parser, multiple_values = true)]
+    din: Option<Vec<String>>,
 }
 
-fn create_files(args: &Vec<String>){
-    for i in 1..args.len(){
-        if args[i].contains("/") && args[i].ends_with("/"){
-                fs::create_dir_all(&args[i])
+
+impl Args {
+    fn input_type(&self) -> InputType {
+        if let Some(_) = self.din {
+            return InputType::DeleteIn;
+        } else if let Some(_) = self.del {
+            return InputType::Del;
+        } else if let Some(_) = self.r#in {
+            return InputType::CreateIn;
+        } else if let Some(_) = self.target {
+            return InputType::Create;
+        } else {
+            unreachable!()
+        }
+    }
+}
+
+#[derive(Debug)]
+enum InputType {
+    DeleteIn,
+    CreateIn,
+    Del,
+    Create,
+}
+
+fn create_files(args: &Args) {
+    let args = args.target.clone().unwrap();
+    for i in 0..args.len() {
+        if args[i].contains("/") && args[i].ends_with("/") {
+            fs::create_dir_all(&args[i])
                 .expect(format!("error creating folder: {}", args[i]).as_str());
-        }
-        else{
-            fs::File::create(&args[i])
-            .expect(format!("error creating file: {}", args[i]).as_str());
-
+        } else {
+            fs::File::create(&args[i]).expect(format!("error creating file: {}", args[i]).as_str());
         }
     }
 }
-fn delete_files(args: &Vec<String>){
-    for i in 2..args.len(){
-        if args[i].contains("/") && args[i].ends_with("/"){
+fn delete_files(args: &Args) {
+    let args = args.del.clone().unwrap();
+    for i in 0..args.len() {
+        if args[i].contains("/") && args[i].ends_with("/") {
             fs::remove_dir_all(&args[i])
-            .expect(format!("error deleting folder: {}", args[i]).as_str());
-        }
-        else{
-            fs::remove_file(&args[i])
-            .expect(format!("error deleting file: {}", args[i]).as_str());
+                .expect(format!("error deleting folder: {}", args[i]).as_str());
+        } else {
+            fs::remove_file(&args[i]).expect(format!("error deleting file: {}", args[i]).as_str());
         }
     }
 }
-fn create_directory(args: &Vec<String>){
-    for i in 2..args.len(){
-        fs::create_dir_all(&args[i])
-        .expect(format!("error creating folder: {}", args[i]).as_str());
+fn create_directory(args: &Args) {
+    let args = args.target.clone().unwrap();
+    for i in 0..args.len() {
+        fs::create_dir_all(&args[i]).expect(format!("error creating folder: {}", args[i]).as_str());
     }
 }
 
-fn main(){
-    let args: Vec<String> = env::args().collect();
-
-    match args[1].as_str(){
-       "-d"|"-delete" => delete_files(&args),
-       "-h"|"-help" => help_message(),
-       "-din" => in_directory::delete_files_dir(&args),
-       "-in" => in_directory::create_in_dir(&args),
-       "-dir" => create_directory(&args),
-       _ => create_files(&args)
+fn main() {
+    let args = Args::parse();
+    match args.input_type() {
+        InputType::DeleteIn => in_directory::delete_files_dir(&args),
+        InputType::CreateIn => in_directory::create_in_dir(&args),
+        InputType::Del => delete_files(&args),
+        InputType::Create => create_files(&args),
     }
-
-
-    
 }
+
