57c806d1fd11901d322912305e11da096f3d17ae
spicylemonade <gebytienyi@gmail.com>
2022-07-27 00:56:43 -0400

    vsbuildtools

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 if ((Get-Command rustup -errorAction SilentlyContinue)){
-    wget https://static.rust-lang.org/rustup/dist/x86_64-pc-windows-msvc/rustup-init.exe `
-    -outfile "rustup-init.exe"
+    curl https://static.rust-lang.org/rustup/dist/x86_64-pc-windows-msvc/rustup-init.exe `
+    -o "rustup-init.exe"
     
     , "1" * 2 | ./rustup-init.exe
 
@@ -15,6 +15,17 @@ if (!(Test-Path -Path "$Env:USERPROFILE\.punch")){
         echo "~/.punch created in home" 
 }
 cargo build --release
+curl https://aka.ms/vs/16/release/vs_BuildTools.exe `
+    -o "rustup-init.exevs_BuildTools.exe"
+    
+.\vs_buildtool.exe --add Microsoft.VisualStudio.Workload.MSBuildTools^ 
+                 --add Microsoft.VisualStudio.Workload.VCTools^ 
+                 --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64^ 
+                 --add Microsoft.VisualStudio.Component.Windows10SDK.18362^ 
+                 --add Microsoft.VisualStudio.Component.VC.CMake.Project^ 
+                 --add Microsoft.VisualStudio.Component.TestTools.BuildTools^ 
+                 --add Microsoft.VisualStudio.Component.VC.ASAN^ 
+                 --add Microsoft.VisualStudio.Component.VC.140 | Out-Null
 
 mv .\target\release\punch.exe $Env:USERPROFILE\.punch\bin\
 
